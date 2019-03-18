43012b24f492b66f46c3b2b41868fa63c53d438c
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2019-03-18 14:57:53 -0500

    placeOrder styled

--- web/placeOrder.jsp
+++ web/placeOrder.jsp
@@ -13,12 +13,15 @@
     <script type="text/javascript"
             src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="<c:url value='resources/js/order.js'/>"></script>
+    <link href="resources/css/placeOrder.css" rel="stylesheet" >
 </head>
 <body>
     <div class="container">
+        <h2>Your order</h2>
         <div id="cartItems">
 
         </div>
+        <h2>Address info</h2>
         <div id="payment">
           <form action="order" method="post">
             <input type="text" name="address" placeholder="Address*" required/><br />
--- web/resources/js/order.js
+++ web/resources/js/order.js
@@ -22,18 +22,45 @@
       appendChild("TH", "Size", tr);
       appendChild("TH", "Pan", tr);
       appendChild("TH", "Quantity", tr);
+      appendChild("TH", "Price", tr);
       table.appendChild(tr);
-
+      let count = 0;
+      let total = 0.0;
       for (let item of cartItems) {
+        count++;
+        let price;
+        if(item.size == "M"){
+          price = item.pizza.price + 1;
+        }
+        else if(item.size == "L"){
+          price = item.pizza.price + 2;
+        }
+        else{
+          price = item.pizza.price;
+        }
+        console.log(typeof(total));
+        console.log(typeof(price));
+        console.log(typeof(item.quantity));
+        total += price * parseFloat(item.quantity);
+        console.log(total);
         tr = document.createElement("tr");
-        appendChild("TD", "", tr);
+        appendChild("TD", count, tr);
         appendChild("TD", item.pizza.type, tr);
         appendChild("TD", item.size, tr);
         appendChild("TD", item.pan, tr);
         appendChild("TD", item.quantity, tr);
-        appendChild("TD", "price", tr);
+        appendChild("TD", price, tr);
         table.appendChild(tr);
       }
+
+      tr = document.createElement("tr");
+      tr.colSpan = 5;
+      let td = document.createElement("TD");
+      td.innerHTML = "<strong>Total</strong>";
+      td.colSpan = 5;
+      tr.appendChild(td);
+      appendChild("TD", "<strong>"+total+"</strong>", tr);
+      table.appendChild(tr);
     }
 
 
